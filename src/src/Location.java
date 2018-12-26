c60964e908bc39a205c81e0a0b701319e0f7679d
Marius <marius.diekhoefer@gmx.de>
2018-12-26 01:09:37 +0100

    changed

--- src/src/Location.java
+++ src/src/Location.java
@@ -10,35 +10,35 @@ public class Location implements src.Interface.Location {
 
     private HashMap<String, Location> neighborMap;
 
-
     public Game getGame() {
         return game;
-    };
+    }
 
     public String getName() {
         return name;
-    };
+    }
 
     public Location getNeighborLocation(String direction) {
-        if(neighborMap.containsKey(direction)) {
+        if (neighborMap.containsKey(direction)) {
             return neighborMap.get(direction);
-        }else{
+        } else {
             System.out.println("No location in direction " + direction + ", returning NULL");
-            return NULL;
+            return null;
         }
-    };
+    }
 
     public void setName(String name) {
         this.name = name;
-    };
+    }
 
-    public void setNeighborLocation(String direction, Location location) {
-        neighborMap.put(direction, location);
-    };
+
+    public void setNeighborLocation(String direction, src.Interface.Location location) {
+        neighborMap.put(direction, (Location) location);
+    }
 
     public Location(String name, Game game) {
         this.name = name;
         this.game = game;
         neighborMap = new HashMap<String, Location>();
-    };
+    }
 }
