5433555363c967ce15992a3dc77810a3ffa27941
Regi9534 <marius.diekhoefer@gmx.de>
2019-01-05 22:16:33 +0100

    Add Inventory to player and location

--- src/src/Inventory.java
+++ src/src/Inventory.java
@@ -1,4 +1,32 @@
 package src;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class Inventory {
+    ConsoleEnviroment consoleEnviroment = new ConsoleEnviroment();
+    ArrayList<Item> conten = new ArrayList();
+    Game game;
+
+    public Inventory(Game game) {
+        this.game = game;
+    }
+
+    public List<Item> getContent() {
+        return this.conten;
+    }
+
+    public void addItemToContent(Item item) {
+        conten.add(item);
+        consoleEnviroment.printOut("");
+        //Inventory have add item
+    }
+
+    public void removeItemFromContent(Item item) {
+        if (conten.contains(item))
+            conten.remove(item);
+        consoleEnviroment.printOut("");
+        //Inventory remove item
+    }
 }
