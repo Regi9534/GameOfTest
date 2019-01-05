1c905177e9463640ce768c00a868958ff73a8c09
Hexxergon <philipboenke@hotmail.de>
2019-01-05 22:23:48 +0100

    added new class Item

---
+++ src/src/Item.java
@@ -0,0 +1,34 @@
+package src;
+
+public class Item {
+    private String name;
+    private String description;
+
+    public Item(String name, String description) {
+        this.name=name;
+        this.description=description;
+    }
+
+    public Item(String name) {
+        new Item(name, "");
+    }
+
+    public Item() {
+        new Item("", "");
+    }
+    public String getDescription() {
+        return description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
