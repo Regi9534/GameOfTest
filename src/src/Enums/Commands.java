820e1e7b9602001f1cf0f9db117c6df2c6869e08
Regi9534 <marius.diekhoefer@gmx.de>
2019-01-06 01:47:03 +0100

    T

--- src/src/Enums/Commands.java
+++ src/src/Enums/Commands.java
@@ -2,11 +2,10 @@ package src.Enums;
 
 public enum Commands {
     MOVE("<move> <direction> you can move to a neighbor of the current location"),
-    AT("Gives the player's current location"),
-    LOOK("Shows all possible directions and items in location"),
-    INVENTORY("list all items in your invetory"),
-    HELP("Give all existent Commands back");
-
+    AT("<at> Gives the player's current location"),
+    LOOK("<look> Shows all possible directions and items in location"),
+    INVENTORY("<inventory> List all items in your invetory"),
+    HELP("<help> Give all existent Commands back");
 
     private String desc;
 
